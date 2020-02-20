ac03617a882ad98e55c8c9a7096b439277c663e7
naigg <gian.andres@gmail.com>
2020-02-20 15:39:34 +1100

    Add the new changes for creating a reducer to listen for show/hide states

--- src/index.js
+++ src/index.js
@@ -1,19 +1,83 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useReducer, createContext } from "react";
+
+const initialState = {
+  display: false
+};
+
+const SHOW_DISPLAY = "show-display";
+const HIDE_DISPLAY = "hide-display";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SHOW_DISPLAY:
+      return {
+        ...state,
+        display: true
+      };
+    case HIDE_DISPLAY:
+      return {
+        ...state,
+        display: false
+      };
+    default:
+      return state;
+  }
+};
+
+/*
+Create a context to listen for changes
+*/
+export const BusyIndicatorContext = createContext({});
+
+/*
+Component about the content
+*/
+const BusyIndicatorDisplay = ({
+  displayStyles,
+  displayContent = "loading"
+}) => {
+  const displayStyle = {
+    position: "absolute",
+    top: 0,
+    ...displayStyles
+  };
+
+  return (
+    <div className="busy-indicator-display" style={displayStyle}>
+      {displayContent}
+    </div>
+  );
+};
 
 /*
 Component so far
 */
-const BusyIndicator = ({ test }) => {
-  return (
-    <div>
-      BusyIndicator {test}
-    </div>
-  )
-};
+const BusyIndicator = ({
+  children,
+  displayStyles,
+  displayContent,
+  ...extraProps
+}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-BusyIndicator.propTypes = {
-  test: PropTypes.bool
+  const showDisplay = () => dispatch({ type: SHOW_DISPLAY });
+  const hideDisplay = () => dispatch({ type: HIDE_DISPLAY });
+
+  const providerValue = { state, showDisplay, hideDisplay };
+
+  return (
+    <BusyIndicatorContext.Provider value={providerValue}>
+      {children}
+      {state && state.display ? (
+        <BusyIndicatorDisplay
+          displayStyles={displayStyles}
+          displayContent={displayContent}
+        />
+      ) : (
+        ""
+      )}
+    </BusyIndicatorContext.Provider>
+  );
 };
 
 export default BusyIndicator;
